04ddd2879422b2af0d111046c8f7c40a5bba3ebc
MoazzemHossain <moazzemh222@gmail.com>
2024-09-18 19:57:09 +0600

    Calculator and Dice Game

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+
+            int result;
+
+            string answer;
+
+            Console.WriteLine("Hello! Welcome to the calculator program!\n");
+            Console.WriteLine("Please enter your first number");
+
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nPlease enter your 2nd number.");
+
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nWhat type of operation would you like to do?");
+            Console.WriteLine("\nPleae enter a for addition, s for subtraction, m for multiplication or d for division.");
+
+            answer = Console.ReadLine();
+
+            if(answer == "a")
+            {
+                result = num1 + num2;
+            }
+            else if(answer == "s")
+            {
+                result = num1 - num2;
+            }
+            else if(answer == "m")
+            {
+                result = num1 * num2;
+            }
+            else
+            {
+                result = num1 / num2;
+            }
+
+            Console.WriteLine("\nThe result is " + result);
+            Console.WriteLine("\nThank you for using the calculator program!");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Dice Game/Dice Game/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int playerRandomNum;
+            int enemyRandomNum;
+
+            int playerPoint = 0;
+            int enemyPoint = 0;
+
+            Random random = new Random();
+
+            for(int i=0; i<5; i++)
+            {
+                Console.WriteLine("\n----------------------------------");
+                Console.WriteLine("\nPlease any key to roll the dice.\n");
+
+                Console.ReadKey();
+
+                playerRandomNum = random.Next(1, 7);
+                Console.WriteLine("\nYou rolled a " + " " + playerRandomNum);
+
+                System.Threading.Thread.Sleep(500);
+
+                enemyRandomNum = random.Next(1, 7);
+                Console.WriteLine("Enemy AI rolled a " + enemyRandomNum);
+
+
+                if(playerRandomNum > enemyRandomNum)
+                {
+                    playerPoint++;
+                    Console.WriteLine("You wins this round!");
+                }
+                else if(playerRandomNum < enemyRandomNum)
+                {
+                    enemyPoint++;
+                    Console.WriteLine("Enemy wins this round!");
+                }
+                else
+                {
+                    Console.WriteLine("Draw!");
+                }
+
+                Console.WriteLine("The score is now= You: " + playerPoint + " and " + "Enemy:" + enemyPoint);
+            }
+
+            if(playerPoint > enemyPoint)
+            {
+                Console.WriteLine("\n\n################################################");
+                Console.WriteLine("######### Congratulations!!! Your win!!! ########");
+                Console.WriteLine("################################################");
+            }
+
+            else if(playerPoint < enemyPoint)
+            {
+                Console.WriteLine("\n\n########################################################");
+                Console.WriteLine("##### Enemy win! Please don't be sad, try again... #####");
+                Console.WriteLine("########################################################");
+            }
+            else
+            {
+                Console.WriteLine("\n\n########################################################");
+                Console.WriteLine("######################## It's a draw! ###################");
+                Console.WriteLine("########################################################");
+            }
+            Console.ReadKey();
+        }
+    }
+}
