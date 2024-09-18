7f2edd566e947c488aba23789bb8a6e7cab0fb91
MoazzemHossain <moazzemh222@gmail.com>
2024-09-18 21:35:11 +0600

    added Shape Area Project

--- ShapeArea/ShapeArea/Program.cs
+++ ShapeArea/ShapeArea/Program.cs
@@ -10,6 +10,43 @@ namespace ShapeArea
     {
         static void Main(string[] args)
         {
+            string answer;
+            float result;
+
+            Console.WriteLine("What shape would you like to find the area of? Please enter r for rectangle or enter t for triangle or enter any other key for circle");
+            answer = Console.ReadLine();
+
+            if(answer == "r")
+            {
+                Console.WriteLine("Please enter the heigh of the rectangle.");
+                float height = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please enre the width of the rectangle.");
+                float width = float.Parse(Console.ReadLine());
+
+                result = height * width;
+            }
+            else if (answer == "t")
+            {
+                Console.WriteLine("Please enter the heigh of the rectangle.");
+                float height = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please enre the width of the rectangle.");
+                float width = float.Parse(Console.ReadLine());
+
+                result = (float)0.5 * height * width;
+            }
+            else
+            {
+                Console.WriteLine("Please enter radius of the circle");
+                float radius = float.Parse(Console.ReadLine());
+
+                result = (float)Math.PI * (radius * radius);
+            }
+
+            Console.WriteLine("The result is: " + result);
+            Console.ReadKey();
         }
+
     }
 }
